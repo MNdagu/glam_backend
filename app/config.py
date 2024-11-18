b05154b43ea82f6dca1070c40fd7900a8c435b8a
MNdagu <ndagumark@gmail.com>
2024-11-18 12:12:19 +0300

    config

--- app/config.py
+++ app/config.py
@@ -1,5 +1,5 @@
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///./ecommerce.db'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://glam_postgres_user:0GHsLJqODkL25mhZp7HFMrXkbty0gtKv@dpg-cstg60jtq21c73aai4n0-a.oregon-postgres.render.com/glam_postgres'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = 'LSMKEBWN NK3JNDUNUO8J'  
